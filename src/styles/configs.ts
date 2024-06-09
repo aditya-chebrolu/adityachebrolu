3b1d1831f96d2aa7a992540458a87f99911d2cf8
Aditya Chebrolu <thisisadityachebrolu@gmail.com>
2024-06-09 21:44:19 +0530

    cc

---
+++ src/styles/configs.ts
@@ -0,0 +1,16 @@
+export const underlinedTextSizeStyles = {
+  dweb: {
+    xs: ["25px", "8px", "-6px"],
+    sm: ["30px", "9.5px", "-7px"],
+    md: ["40px", "12px", "-9px"],
+    lg: ["50px", "15px", "-11px"],
+    xl: ["80px", "24px", "-17.5px"],
+  },
+  mweb: {
+    xs: ["18px", "6px", "-4.5px"],
+    sm: ["25px", "8px", "-6px"],
+    md: ["30px", "9.5px", "-7px"],
+    lg: ["40px", "12px", "-9px"],
+    xl: ["40px", "12px", "-9px"],
+  },
+};
